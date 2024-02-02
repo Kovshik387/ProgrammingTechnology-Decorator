6a3f9a0c854100500731389ca212594a590d4505
Kovshik387 <fantokin03@mail.ru>
2024-02-02 13:12:50 +0300

    init

---
+++ src/main/java/com/example/programmingtehnologydecorator/service/Star.java
@@ -0,0 +1,41 @@
+package com.example.programmingtehnologydecorator.service;
+
+import com.example.programmingtehnologydecorator.models.IChristmasTree;
+import com.example.programmingtehnologydecorator.models.TreeDecorator;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+
+public class Star extends TreeDecorator {
+    public Star(IChristmasTree tree) {
+        super(tree);
+    }
+
+    @Override
+    public void draw(Pane paneStar) {
+        super.draw(paneStar);
+        this.drawStar(paneStar);
+    }
+
+    private void drawStar(Pane paneStar) {
+        var path = new Path() {{
+            setFill(Color.GOLD);
+            setFillRule(FillRule.EVEN_ODD);
+            setStroke(Color.YELLOW);
+        }};
+        path.getElements().addAll(
+                new MoveTo(100, 0),
+                new LineTo(108, 20),
+                new LineTo(125, 20),
+                new LineTo(114, 34),
+                new LineTo(118, 50),
+                new LineTo(100, 40),
+                new LineTo(82, 50),
+                new LineTo(86, 34),
+                new LineTo(75, 20),
+                new LineTo(92, 20),
+                new ClosePath()
+        );
+        paneStar.getChildren().addAll(path);
+    }
+}
